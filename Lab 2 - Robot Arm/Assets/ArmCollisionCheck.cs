6556b17ea43283f777b9262b6b78ab447e3877f6
Jason <jasondien@gmail.com>
2020-08-24 10:39:14 -0700

    initial commit

---
+++ Lab 2 - Robot Arm/Assets/ArmCollisionCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmCollisionCheck : MonoBehaviour
+{
+    public GameObject TargetSpawner;
+    
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Target") {
+            RandomSpawn rs = TargetSpawner.GetComponent<RandomSpawn>();
+            rs.OnTargetHit();
+        }
+    }
+}
